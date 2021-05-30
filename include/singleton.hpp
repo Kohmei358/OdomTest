9c3ba61dd0ed0c910ffcf465d4a604b434aac579
Kohmei Kadoya <kkadoya@wpi.edu>
2021-05-30 16:39:18 -0400

    Initial commit

---
+++ include/singleton.hpp
@@ -0,0 +1,47 @@
+#ifndef Chassis_hpp
+#define Chassis_hpp
+#include "main.h"
+
+using namespace std;
+
+class Singleton {
+   static Singleton *instance;
+   shared_ptr<OdomChassisController> chassis;
+
+   // Private constructor so that no objects can be created.
+   Singleton() {
+     chassis =
+       ChassisControllerBuilder()
+         .withMotors({-20,14,-12},{2,-6,10}) // first set is left, 2nd set is right (- means reversed)
+         .withGains(
+             {0.0004, 0.000, 0.000025}, // distance controller gains
+             {0.00135, 0.0002, 0.000037}, // turn controller gains
+             {0.000135, 0.000, 0.0000}  // angle controller gains (helps drive straight)
+         )
+         .withSensors(
+             ADIEncoder{'C', 'D', false}, // left encoder in ADI ports A & B
+             ADIEncoder{'E', 'F', true},  // right encoder in ADI ports C & D (reversed)
+             ADIEncoder{'A', 'B', false}  // middle encoder in ADI ports E & F
+         )
+         // green gearset, tracking wheel diameter (3.24 in), track (7.32 in), and TPR (256(PPR)*4)
+         // 4.5 inch middle encoder distance, and 2.75 inch middle wheel diameter
+         // 4.3
+         .withDimensions(AbstractMotor::gearset::green, {{3.30_in, 7.20_in, 4.3_in, 2.80_in}, 1024})
+         .withOdometry() // use the same scales as the chassis (above)
+         .buildOdometry(); // build an odometry chassis
+   }
+
+   public:
+   static Singleton *getInstance() {
+      if (!instance)
+      instance = new Singleton;
+      return instance;
+   }
+
+   shared_ptr<OdomChassisController> getChassis() {
+   return this -> chassis;
+}
+};
+
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,168 @@
+#include "main.h"
+#include "singleton.hpp"
+
+//Initialize pointer to zero so that it can be initialized in first call to getInstance
+Singleton *Singleton::instance = 0;
+
+void displayTaskFnc(){
+	Singleton *s = s->getInstance();
+	shared_ptr<OdomChassisController> chassis = s->getChassis();
+
+	Controller controller;
+	controller.clear();
+
+	while(true){
+
+		char buff[100];
+		char buff2[100];
+		snprintf(buff, sizeof(buff), "(%3.1f,%3.1f)",chassis->getState().x.convert(inch),chassis->getState().y.convert(inch));
+		snprintf(buff2, sizeof(buff2), "T:%3.2f",chassis->getState().theta.convert(degree));
+
+		std::string buffAsStdStr = buff;
+		std::string buff2AsStdStr = buff2;
+		controller.setText(1,3, buff2);
+
+		pros::lcd::print(0, "X: %f", chassis->getState().x.convert(inch));
+		pros::lcd::print(1, "Y: %f", chassis->getState().y.convert(inch));
+		pros::lcd::print(2, "T: %f", chassis->getState().theta.convert(degree));
+
+		pros::lcd::print(4, "L: %d", chassis->getModel()->getSensorVals()[0]);
+		pros::lcd::print(5, "R: %d", chassis->getModel()->getSensorVals()[1]);
+		pros::lcd::print(6, "B: %d", chassis->getModel()->getSensorVals()[2]);
+
+		pros::delay(10);
+	}
+}
+
+/**
+ * A callback function for LLEMU's center button.
+ *
+ * When this callback is fired, it will toggle line 2 of the LCD text between
+ * "I was pressed!" and nothing.
+ */
+void on_center_button() {
+	static bool pressed = false;
+	pressed = !pressed;
+	if (pressed) {
+		pros::lcd::set_text(2, "I was pressed!");
+	} else {
+		pros::lcd::clear_line(2);
+	}
+}
+
+/**
+ * Runs initialization code. This occurs as soon as the program is started.
+ *
+ * All other competition modes are blocked by initialize; it is recommended
+ * to keep execution time for this mode under a few seconds.
+ */
+void initialize() {
+	pros::lcd::initialize();
+	Singleton *s = s->getInstance();
+}
+
+/**
+ * Runs while the robot is in the disabled state of Field Management System or
+ * the VEX Competition Switch, following either autonomous or opcontrol. When
+ * the robot is enabled, this task will exit.
+ */
+void disabled() {}
+
+/**
+ * Runs after initialize(), and before autonomous when connected to the Field
+ * Management System or the VEX Competition Switch. This is intended for
+ * competition-specific initialization routines, such as an autonomous selector
+ * on the LCD.
+ *
+ * This task will exit when the robot is enabled and autonomous or opcontrol
+ * starts.
+ */
+void competition_initialize() {}
+
+/**
+ * Runs the user autonomous code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the autonomous
+ * mode. Alternatively, this function may be called in initialize or opcontrol
+ * for non-competition testing purposes.
+ *
+ * If the robot is disabled or communications is lost, the autonomous task
+ * will be stopped. Re-enabling the robot will restart the task, not re-start it
+ * from where it left off.
+ */
+void autonomous() {
+
+}
+
+/**
+ * Runs the operator control code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the operator
+ * control mode.
+ *
+ * If no competition control is connected, this function will run immediately
+ * following initialize().
+ *
+ * If the robot is disabled or communications is lost, the
+ * operator control task will be stopped. Re-enabling the robot will restart the
+ * task, not resume it from where it left off.
+ */
+void opcontrol() {
+
+
+		pros::Task displayTask(displayTaskFnc);
+
+		Controller controller;
+		controller.clear();
+		ControllerButton runAutoButton(ControllerDigital::X);
+		ControllerButton AButton(ControllerDigital::A);
+		ControllerButton BButton(ControllerDigital::B);
+		Singleton *s = s->getInstance();
+		shared_ptr<OdomChassisController> chassis = s->getChassis();
+
+		std::shared_ptr<AsyncMotionProfileController> profileController =
+		AsyncMotionProfileControllerBuilder()
+			.withLimits({
+				1.0, // Maximum linear velocity of the Chassis in m/s
+				2.0, // Maximum linear acceleration of the Chassis in m/s/s
+				10.0 // Maximum linear jerk of the Chassis in m/s/s/s
+			})
+			.withOutput(chassis)
+			.buildMotionProfileController();
+
+		profileController->generatePath(
+    {{0_ft, 0_ft, 0_deg}, {3_ft, 0_ft, 0_deg}}, "A");
+
+		profileController->generatePath(
+		{{3_ft, 0_ft, 0_deg}, {0_ft, 0_ft, 0_deg}}, "B");
+
+		while (true) {
+		// Arcade drive with the left stick
+		chassis->getModel()->arcade(controller.getAnalog(ControllerAnalog::rightY),
+															controller.getAnalog(ControllerAnalog::leftX));
+
+		// Run the test autonomous routine if we press the button
+		if (runAutoButton.changedToPressed()) {
+				// Drive the robot in a square pattern using closed-loop control
+				chassis->turnAngle(180_deg);   // Turn in place 90 degrees
+		}
+
+		if (AButton.changedToPressed()) {
+					// chassis->moveDistance(24_in); // Drive forward 12 inches
+					profileController->setTarget("A");
+					profileController->waitUntilSettled();
+
+					// chassis->turnAngle(90_deg);   // Turn in place 90 degrees
+		}
+
+		if (BButton.changedToPressed()) {
+				// chassis->moveDistance(-24_in); // Drive forward 12 inches
+				// chassis->turnAngle(90_deg);   // Turn in place 90 degrees
+				profileController->setTarget("B");
+				profileController->waitUntilSettled();
+		}
+		// Wait and give up the time we don't need to other tasks.
+		// Additionally, joystick values, motor telemetry, etc. all updates every 10 ms.
+		pros::delay(10);
+	}
+}
